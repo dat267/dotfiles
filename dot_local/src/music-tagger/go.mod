645a3540ea4fb2532bd2b167d16c91673f6f2cec
Dat Do <dat267@outlook.com>
2026-06-14 03:28:23 +0700

    feat(tools): add music-tagger Go tool

---
+++ dot_local/src/music-tagger/go.mod
@@ -0,0 +1,11 @@
+module music-tagger
+
+go 1.25.0
+
+require (
+	github.com/bogem/id3v2/v2 v2.1.4
+	github.com/go-flac/flacvorbis/v2 v2.0.2
+	github.com/go-flac/go-flac/v2 v2.0.4
+)
+
+require golang.org/x/text v0.3.8 // indirect
---
+++ dot_local/src/music-tagger/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bogem/id3v2/v2"
+	"github.com/go-flac/flacvorbis/v2"
+	"github.com/go-flac/go-flac/v2"
+)
+
+var version = "dev"
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: music-tagger <directory>")
+		os.Exit(1)
+	}
+	if os.Args[1] == "-v" || os.Args[1] == "--version" {
+		fmt.Println("music-tagger version", version)
+		os.Exit(0)
+	}
+	root := os.Args[1]
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return nil
+		}
+		ext := strings.ToLower(filepath.Ext(path))
+		if ext == ".mp3" || ext == ".flac" {
+			tagFile(path, ext)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+func tagFile(path string, ext string) {
+	abs, err := filepath.Abs(path)
+	if err != nil {
+		return
+	}
+	parts := strings.Split(abs, string(filepath.Separator))
+	if len(parts) < 3 {
+		return
+	}
+	album := parts[len(parts)-2]
+	artist := parts[len(parts)-3]
+	filename := strings.TrimSuffix(parts[len(parts)-1], filepath.Ext(path))
+	track := ""
+	title := filename
+	if idx := strings.Index(filename, "__"); idx != -1 {
+		artist = strings.TrimSpace(filename[idx+2:])
+		filename = strings.TrimSpace(filename[:idx])
+	}
+	if idx := strings.Index(filename, "-"); idx != -1 {
+		track = strings.TrimSpace(filename[:idx])
+		title = strings.TrimSpace(filename[idx+1:])
+	}
+	if ext == ".mp3" {
+		tagMP3(path, artist, album, title, track)
+		return
+	}
+	if ext == ".flac" {
+		tagFLAC(path, artist, album, title, track)
+	}
+}
+
+func tagMP3(path, artist, album, title, track string) {
+	tag, err := id3v2.Open(path, id3v2.Options{Parse: true})
+	if err != nil {
+		return
+	}
+	defer tag.Close()
+	compatibleArtist := strings.ReplaceAll(artist, ";", " / ")
+	tag.SetArtist(compatibleArtist)
+	tag.SetAlbum(album)
+	tag.SetTitle(title)
+	if track != "" {
+		tag.AddTextFrame(tag.CommonID("Track number"), id3v2.EncodingUTF8, track)
+	}
+	tag.Save()
+}
+
+func findVorbisComment(meta []*flac.MetaDataBlock) (*flacvorbis.MetaDataBlockVorbisComment, int) {
+	for idx, block := range meta {
+		if block.Type != flac.VorbisComment {
+			continue
+		}
+		cmt, err := flacvorbis.ParseFromMetaDataBlock(*block)
+		if err == nil {
+			return cmt, idx
+		}
+	}
+	return flacvorbis.New(), -1
+}
+
+func tagFLAC(path, artist, album, title, track string) {
+	f, err := flac.ParseFile(path)
+	if err != nil {
+		return
+	}
+	cmt, cmtIdx := findVorbisComment(f.Meta)
+	artists := strings.Split(artist, ";")
+	for _, a := range artists {
+		cmt.Add(flacvorbis.FIELD_ARTIST, strings.TrimSpace(a))
+	}
+	cmt.Add(flacvorbis.FIELD_ALBUM, album)
+	cmt.Add(flacvorbis.FIELD_TITLE, title)
+	if track != "" {
+		cmt.Add(flacvorbis.FIELD_TRACKNUMBER, track)
+	}
+	cmtsmeta := cmt.Marshal()
+	if cmtIdx >= 0 {
+		f.Meta[cmtIdx] = &cmtsmeta
+	} else {
+		f.Meta = append(f.Meta, &cmtsmeta)
+	}
+	f.Save(path)
+}
